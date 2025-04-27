ce5009c9c12e70fe8547a805ae476af17e47eff9
liujunfeng <liujunfeng@corp.netease.com>
2025-04-27 17:01:32 +0800

    修改setup

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pyJianYingDraft",
+    name="jianyingdraft",
     version="0.1.1",
-    author="gary318",
+    author="doumai",
     description="轻量、灵活、易上手的Python剪映草稿生成及导出工具，构建全自动化视频剪辑/混剪流水线",
-    long_description=open("pypi_readme.md", "r", encoding="utf-8").read(),
+    long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/GuanYixuan/pyJianYingDraft",
+    url="https://github.com/doumai-0130/jianyingdraft",
     packages=find_packages(),
     package_data={
-        'pyJianYingDraft': ['draft_content_template.json']
+        'jianyingdraft': ['draft_content_template.json']
     },
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -21,7 +21,6 @@ setup(
     python_requires='>=3.8',
     install_requires=[
         "pymediainfo",
-        "imageio",
-        "uiautomation>=2"
+        "imageio"
     ],
 )
